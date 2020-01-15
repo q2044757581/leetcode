d43438902780bd86ab859d64b080cd59a37adbee
q2044757581 <32102666+q2044757581@users.noreply.github.com>
2020-01-15 12:06:22 +0800

    ll

---
+++ 1、两数之和/code.py
@@ -0,0 +1,13 @@
+
+
+class Solution:
+    def twoSum(self, nums, target):
+        Map = {}
+        for i in range(len(nums)):
+            if target - nums[i] in Map:
+                return [Map[target - nums[i]], i]
+            Map[nums[i]] = i
+
+
+t = Solution()
+print(t.twoSum([2, 7, 11, 15], 9))
